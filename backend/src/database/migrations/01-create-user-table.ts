8f61ee16eebfa3d7848bcca8ccd543d733653ae5
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-07-27 00:07:37 -0300

    Iniciando as configurações do projeto

---
+++ backend/src/database/migrations/01-create-user-table.ts
@@ -0,0 +1,34 @@
+import { DataTypes, Model, QueryInterface } from "sequelize";
+import IUser from "../../Interfaces/User/User";
+
+export default {
+  up(queryInterface : QueryInterface) {
+    return queryInterface.createTable<Model<IUser>>('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      fullName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    })
+  },
+  down(queryInterface : QueryInterface) {
+    return queryInterface.dropTable('users');
+  },
+}
