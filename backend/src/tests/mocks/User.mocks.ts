5c2debd63e8a27830de64d05f2dc59cdf1323c27
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-08-10 16:39:10 -0300

    Escrevendo os testes da rota 'user'

---
+++ backend/src/tests/mocks/User.mocks.ts
@@ -0,0 +1,20 @@
+const user = {
+  id: 1,
+  fullName: 'Samuel Morais',
+  email: 'moraissamuel009@gmail.com',
+  password: 'S@mmeel2341',
+  phone: '38988449448',
+}
+
+const loginUser = {
+  id: 1,
+  fullName: 'Samuel Morais',
+  email: 'admin@admin.com',
+  password: 'S@muel123',
+  phone: '38988449448',
+}
+
+export {
+  user,
+  loginUser,
+}
